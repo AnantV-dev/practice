dd182c87f4570e02357d8f12409d104fd561b565
AnantV-dev <gandharva1111@gmail.com>
2022-02-27 12:09:36 +0530

    now substraction

--- addition.sh
+++ addition.sh
@@ -4,6 +4,6 @@ echo "Enter first number : "
 read num1
 echo "Enter second number : "
 read num2
-echo "Addition"
-echo "$num1 + $num2"
+echo "Substraction"
+echo "$num1 - $num2"
 echo `expr $num1 + $num2`
