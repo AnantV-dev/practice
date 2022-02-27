da71c3459b4d2a85e40041028bf022ce4ae96656
AnantV-dev <gandharva1111@gmail.com>
2022-02-27 12:38:33 +0530

    code slightly changed

--- compare.sh
+++ compare.sh
@@ -4,13 +4,13 @@ echo "Enter any number : "
 
 read num1
 
-if [ $num1 -lt 10]
+if [ $num1 -lt 10 ]
 then
     echo "This is single digit number."
-elif [[ $num1 -gt 9 && $num1 -lt 100]]
+elif [[ $num1 -gt 9 && $num1 -lt 100 ]]
 then 
     echo "This number is two digit number."
-elif [[ $num1 -gt 99 && $num1 -lt 1000]]
+elif [[ $num1 -gt 99 && $num1 -lt 1000 ]]
 then 
     echo "This is three digit number."
 fi
